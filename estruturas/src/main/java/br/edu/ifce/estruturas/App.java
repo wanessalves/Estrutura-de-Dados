6fcc15b507482f6f2ed64ba304c6336ca7509b2d
Gustavo Coutinho <gustavolgcr@gmail.com>
2018-02-05 21:17:13 -0300

    Reorganizando o projeto.

---
+++ estruturas/src/main/java/br/edu/ifce/estruturas/App.java
@@ -0,0 +1,13 @@
+package br.edu.ifce.estruturas;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
